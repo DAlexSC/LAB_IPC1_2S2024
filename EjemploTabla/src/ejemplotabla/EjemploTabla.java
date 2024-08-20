caf5f8a3302525af056d5590e0f83c191023670e
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-08-20 16:37:39 -0600

    [ADD] Ejemplo de una tabla en el cual algunas celdas contiene un boton

---
+++ EjemploTabla/src/ejemplotabla/EjemploTabla.java
@@ -0,0 +1,23 @@
+/*
+ *  Repositorio: https://github.com/DAlexSC/LAB_IPC1_2S2024
+ */
+
+package ejemplotabla;
+
+import interfaz.MenuAdministrador;
+
+/**
+ * @author Douglas Soch
+ */
+public class EjemploTabla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuAdministrador vistaAdministrador = new MenuAdministrador();
+        vistaAdministrador.setVisible(true);
+        vistaAdministrador.setLocationRelativeTo(null);
+    }
+
+}
